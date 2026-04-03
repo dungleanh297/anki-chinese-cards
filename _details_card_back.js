a1b40acb044dc5fa4bfdd943fdd85d4ab3d2ff8a
dungleanh297 <106744615+dungleanh297@users.noreply.github.com>
2026-04-03 17:09:59 +0700

    Showing HanViet in back of the card

--- _details_card_back.js
+++ _details_card_back.js
@@ -3,8 +3,10 @@
         const childNodeCount = detailsElement.childNodes.length;
         return childNodeCount > 3 || (childNodeCount === 3 && (detailsElement.childNodes[2].nodeType !== Node.TEXT_NODE || detailsElement.childNodes[2].textContent.trim() !== "")); 
     }
-    const exampleDetails = document.getElementById('example');
-    const explanationDetails = document.getElementById('explanation');
+
+    const hanVietSpan = document.getElementById("han-viet");
+    const exampleDetails = document.getElementById("example");
+    const explanationDetails = document.getElementById("explanation");
 
     if (!detailsIsNotEmpty(exampleDetails)) {
         exampleDetails.classList.add("hidden");
@@ -12,5 +14,9 @@
 
     if (!detailsIsNotEmpty(explanationDetails)) {
         explanationDetails.classList.add("hidden");
+    }
+
+    if (hanVietSpan.innerText.length == 2) {
+        hanVietSpan.classList.add("hidden");
     }
 })();
